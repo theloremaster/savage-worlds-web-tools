0add688a7fbab99d63cae7d160d96c74b88d069b
Jeffrey D. Gordon <jdg@gauthic.com>
2014-03-28 09:05:02 -0500

    Added Raise Calculator

---
+++ js/raise_calculator.js
@@ -0,0 +1,24 @@
+function do_success_calc() {
+	roll = $("input[name='roll'").val();
+	roll = roll / 1;
+
+	target_number = $("input[name='target-number'").val();
+	target_number = target_number / 1;
+
+	armor = $("input[name='armor'").val();
+	armor = armor / 1;
+
+	armorpiercing = $("input[name='weapon-ap'").val();
+	armorpiercing = armorpiercing / 1;
+
+
+	damage_html = damageSuccessMargin(roll, target_number, armor, armorpiercing);
+	attribute_html = traitSuccessMargin(roll, target_number);
+	$(".js-results").html( attribute_html );
+	$(".js-damage-results").html( damage_html );
+}
+$(".raise-calc-input").keyup( function() {
+	do_success_calc();
+});
+
+do_success_calc();
